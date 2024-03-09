16b1f3203b690d118fec7f63afe5ffde22836c15
Christian König <ckoenig@posteo.de>
2024-03-09 19:56:50 +0000

    Refactor
    Signed-off-by: Christian König <ckoenig@posteo.de>

---
+++ src/notifications.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"sync"
+	"time"
+)
+
+func sendNotifications(timestamp time.Time, message string, title string) {
+	// Sending messages to different services as goroutines concurrently
+	// Adding a wait group here to delay execution until all functions return,
+	// otherwise delaying in processEvent() would not make any sense
+
+	var wg sync.WaitGroup
+
+	// If there is a server tag, add it to the title
+	if len(glb_arguments.ServerTag) > 0 {
+		title = "[" + glb_arguments.ServerTag + "] " + title
+	}
+
+	if glb_arguments.Pushover {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			sendPushover(message, title)
+		}()
+	}
+
+	if glb_arguments.Gotify {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			sendGotify(message, title)
+		}()
+	}
+
+	if glb_arguments.Mail {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			sendMail(timestamp, message, title)
+		}()
+	}
+	wg.Wait()
+
+}
